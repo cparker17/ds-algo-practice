3c0b74476989e4bbe064f9a260a067f83c054f2c
Chris Parker <chris@parkerdesignbuild.com>
2024-06-02 04:07:40 -0400

    linked list completed

--- ds-algo-practice/src/main/java/org/example/LinkedList.java
+++ ds-algo-practice/src/main/java/org/example/LinkedList.java
@@ -22,6 +22,11 @@ public class LinkedList {
         length = 1;
     }
 
+
+    public int length() {
+        return length;
+    }
+
     public void append(int value) {
         Node node = new Node(value);
         if (head == null) {
@@ -87,5 +92,82 @@ public class LinkedList {
             count++;
         }
         return currentNode;
+    }
+
+    public boolean set(int index, int value) {
+        if (index < 0 || index >= length) {
+            return false;
+        }
+        Node currentNode = head;
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+        currentNode.value = value;
+        return true;
+    }
+
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > length) {
+            return false;
+        } else if (index == 0) {
+            prepend(value);
+            return true;
+        } else if (index == length) {
+            append(value);
+            return true;
+        } else {
+            Node node = new Node(value);
+            Node previous = get(index - 1);
+            node.next = previous.next;
+            previous.next = node;
+            length++;
+            return true;
+        }
+    }
+
+    public void printValues() {
+        if (length == 0) {
+            System.out.println("No values.");
+        }
+        Node currentNode = head;
+        for (int i = 0; i < length; i++) {
+            System.out.print(currentNode.value + " - ");
+            currentNode = currentNode.next;
+        }
+    }
+
+    public Node remove(int index) {
+        if (index < 0 || index >= length) {
+            return null;
+        }
+        if (index == 0) {
+            Node node = get(0);
+            head = node.next;
+            return node;
+        }
+        if (index == length - 1) {
+            Node node = get(index);
+            tail = get(index - 1);
+            return node;
+        }
+        Node node = get(index);
+        Node prev = get(index -1);
+        prev.next = node.next;
+        length--;
+        return node;
+    }
+
+    public void reverse() {
+        Node temp = head;
+        head = tail;
+        tail = temp;
+        Node beforeTemp = null;
+        Node afterTemp;
+        for (int i = 0; i < length; i++) {
+            afterTemp = temp.next;
+            temp.next = beforeTemp;
+            beforeTemp = temp;
+            temp = afterTemp;
+        }
     }
 }
--- ds-algo-practice/src/main/java/org/example/Main.java
+++ ds-algo-practice/src/main/java/org/example/Main.java
@@ -15,5 +15,16 @@ public class Main {
         System.out.println(myLinkedList.get(1).value);
         System.out.println(myLinkedList.get(2).value);
         //System.out.println(myLinkedList.get(4).value);
+//        myLinkedList.set(0, 100);
+//        myLinkedList.set(2, 200);
+//        System.out.println(myLinkedList.get(0).value);
+//        System.out.println(myLinkedList.get(2).value);
+        myLinkedList.insert(2, 500);
+        myLinkedList.printValues();
+        System.out.println("\n");
+        myLinkedList.printValues();
+        myLinkedList.reverse();
+        System.out.println("\n");
+        myLinkedList.printValues();
     }
 }
