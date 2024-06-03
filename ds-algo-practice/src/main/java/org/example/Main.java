7030172b1e2a543c7bdcb79b06c3ddab5ac066b8
Chris Parker <chris@parkerdesignbuild.com>
2024-06-03 13:27:47 -0400

    completed graph

--- ds-algo-practice/src/main/java/org/example/Main.java
+++ ds-algo-practice/src/main/java/org/example/Main.java
@@ -141,5 +141,26 @@ public class Main {
         hashTable.delete("picture", 10);
         hashTable.delete("chair", 75);
         hashTable.printTableValues();
+
+        // ********GRAPH TESTS***********
+
+        System.out.println();
+
+        Graph graph = new Graph();
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.printGraph();
+
+        graph.addEdge("A", "B");
+        graph.addEdge("A", "C");
+        graph.addEdge("B", "C");
+        graph.printGraph();
+
+        graph.removeEdge("A", "B");
+        graph.printGraph();
+
+        graph.removeVertex("C");
+        graph.printGraph();
     }
 }
